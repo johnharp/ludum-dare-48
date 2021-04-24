abe2cf6d90ef0a0ddfa728d0c2fc2614a25d134f
John Harper <john@harper.link>
2021-04-24 17:59:04 -0500

    despawn bubbles

--- divedive/Assets/Scripts/BubbleGenerator.cs
+++ divedive/Assets/Scripts/BubbleGenerator.cs
@@ -18,6 +18,8 @@ public class BubbleGenerator : MonoBehaviour
     private GameController GameController = null;
     private GameObject Player = null;
 
+    List<GameObject> bubbles = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +31,8 @@ public class BubbleGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"bubble: next spawn at {nextSpawnTime}");
+        destroyBubbles();
+
         if (Time.time >= nextSpawnTime)
         {
             int index = Random.Range(0, bubblePrefabs.Length);
@@ -43,7 +46,31 @@ public class BubbleGenerator : MonoBehaviour
                 bubblePrefab,
                 pos,
                 Quaternion.identity);
+            bubbles.Add(newBubble);
             chooseNewNextSpawnTime();
+        }
+    }
+
+    private void destroyBubbles()
+    {
+        List<GameObject> bubblesToDestroy = new List<GameObject>();
+        foreach(var bubble in bubbles)
+        {
+            float dist = Mathf.Abs(
+                bubble.transform.position.y -
+                Player.transform.position.y);
+
+
+            if (dist > despawnDistance)
+            {
+                bubblesToDestroy.Add(bubble);
+            } 
+        }
+
+        foreach(var bubble in bubblesToDestroy)
+        {
+            bubbles.Remove(bubble);
+            Destroy(bubble);
         }
     }
 
